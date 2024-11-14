3900ffdd7be3c5e16359aed27448e127e2ae2201
jigolaurito93 <jigolaurito93@yahoo.com>
2024-11-14 02:19:24 -0600

    Math class

---
+++ mathClass.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class mathClass {
+    public static void main(String[] arg) {
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.max(x,y); // maximum value
+        double q = Math.min(x,y); // minimum value
+        System.out.println(z);
+        System.out.println(q);
+        double p = Math.abs(y); // absolute value
+        System.out.println(p);
+        double r = 16;
+        double s = Math.sqrt(r); // square root
+        System.out.println(s);
+
+       
+    }
+}
